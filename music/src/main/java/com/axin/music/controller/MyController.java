184990f80e3cc7c185dd14b1ea364ec79929bed6
ricarrrrdo <ricarrrrdo@163.com>
2020-02-03 13:54:20 +0800

    也许世界就这样

--- music/src/main/java/com/axin/music/controller/MyController.java
+++ music/src/main/java/com/axin/music/controller/MyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/axin")
 public class MyController {
     
+    //test
     @RequestMapping("/music")
     public String listen() {
         return "/musicDemo.html";
